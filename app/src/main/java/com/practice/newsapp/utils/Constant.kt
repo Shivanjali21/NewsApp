04984ad047761851fd6c1afa0a13535081ee7255
Shivanjali21 <65972893+Shivanjali21@users.noreply.github.com>
2024-07-01 12:51:03 +0530

    Update Constant.kt

--- app/src/main/java/com/practice/newsapp/utils/Constant.kt
+++ app/src/main/java/com/practice/newsapp/utils/Constant.kt
@@ -3,7 +3,7 @@ package com.practice.newsapp.utils
 class Constants {
 
     companion object {
-        const val API_KEY = "caf277e8a3d545128b1df6c3cfa0faf1"
+        const val API_KEY = "caf277e8a3d545128b1df6c3cfa0fa"
         const val BASE_URL = "https://newsapi.org"
         const val SEARCH_NEWS_TIME_DELAY = 500L
         const val QUERY_PAGE_SIZE = 20
